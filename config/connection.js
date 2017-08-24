e4f802befca838119317bcd9357c2c3b59fa2298
Zach Durham <ZachDurham@dhcp-v80-000497.wireless.unc.edu>
2017-08-24 12:13:20 -0400

    added heroku jawsDB code

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql')
+var connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "codingbootcamp",
-  database: "burgers_db"
-})
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "codingbootcamp",
+    database: "burgers_db"
+  })
+}
+
+
 
 
 connection.connect(function(err) {
